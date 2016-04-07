92785e81690d32ef2014eef7866f47e2330221b1
Remo Lüönd <remo.zumsteg@outlook.com>
2016-04-07 14:12:55 +0200

    added build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "---Installing global dependencies:"
+npm install -g typescript
+npm install -g mocha
+npm install -g tsd
+
+cd api
+echo "---API: in"
+pwd
+echo "---Installing local dependencies:"
+npm install
+tsd install
+
+tsc
